7bdbbea23772f1db2b6776997261ee5ad991e561
OYYX369 <102648883+OYYX369@users.noreply.github.com>
2024-07-18 18:08:18 +0800

    add 1

---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 13303
+* @description 针对表【user(用户表)】的数据库操作Service
+* @createDate 2024-06-15 19:07:11
+*/
+public interface UserService extends IService<User> {
+
+}
